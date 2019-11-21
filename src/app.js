f157d8d99378dca23c3880985f567cdfc40344c9
Matt Thornberry <mtberry16@hotmail.com>
2019-11-21 10:56:26 -0700

    Validate bearer token

--- src/app.js
+++ src/app.js
@@ -13,6 +13,15 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const bearerToken = req.get("Authorization");
+
+  if (!bearerToken || apiToken !== bearerToken.split(" ")[1]) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
