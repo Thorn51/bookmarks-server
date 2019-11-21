23688719cdf439dcbf2abae116dc60d8f20b981a
Matt Thornberry <mtberry16@hotmail.com>
2019-11-21 13:27:59 -0700

    Add test data

---
+++ src/store.js
@@ -0,0 +1,24 @@
+const bookmarks = [
+  {
+    id: "91581e2d-4643-4f2f-807d-3e8bf187b5c6",
+    title: "Thinkful",
+    url: "https://www.thinkful.com/",
+    description: "Think outside the classroom",
+    rating: 5
+  },
+  {
+    id: "49c1d7c1-8d73-409c-b7a8-78e7d61c18af",
+    title: "Google",
+    url: "https://www.google.com/",
+    description: "Where we find everything else",
+    rating: 4
+  },
+  {
+    id: "a69a4bfd-4dbe-45c1-ae02-00c1d7f2b3d1",
+    title: "MDN",
+    description: "The only place to find web documentation",
+    rating: 5
+  }
+];
+
+module.exports = bookmarks;
